5c022e9af173578be1d4d1122217a42440e902c0
Christopher Gilbert <chris.gilbert@datasift.com>
2014-07-01 11:18:37 +0100

    Godoc fix

--- event.go
+++ event.go
@@ -1,7 +1,3 @@
-// A Riemann client for Go, featuring concurrency, sending events and state updates, queries,
-// and feature parity with the reference implementation written in Ruby.
-//
-// Copyright (C) 2014 by Christopher Gilbert <christopher.john.gilbert@gmail.com>
 package goryman
 
 import ()
